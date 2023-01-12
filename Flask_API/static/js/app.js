61b02156c171a1da37ec6f389964b7cd09fabfc9
RafSchapendonk <rafschapendonk@outlook.com>
2023-01-12 21:39:15 +0100

    Removed unneeded code

--- Flask_API/static/js/app.js
+++ Flask_API/static/js/app.js
@@ -3,40 +3,6 @@ var images = {
     depth: ''
 }
 
-// function loadPage(route) {
-//     $("#root").load(route, function (statusTxt, jqXHR) {
-//         if (statusTxt == "success") {
-//             alert("New content loaded successfully!");
-//         }
-//         if (statusTxt == "error") {
-//             alert("Error: " + jqXHR.status + " " + jqXHR.statusText);
-//         }
-//     });
-// }
-
-// $(document).ready(function () {
-//     $("#root").load("../templates/home.html", function (statusTxt, jqXHR) {
-//         if (statusTxt == "success") {
-//             alert("New content loaded successfully!");
-//         }
-//         if (statusTxt == "error") {
-//             alert("Error: " + jqXHR.status + " " + jqXHR.statusText);
-//         }
-//     });
-
-//     $("#extraction-button").click(function () {
-//         loadPage('../templates/extraction.html')
-//     });
-
-//     $("#home-button").click(function () {
-//         loadPage('../templates/home.html')
-//     });
-
-//     $("#result-button").click(function () {
-//         loadPage('../templates/result.html')
-//     });
-// });
-
 function loadRGBFile(event) {
     images.rgb = URL.createObjectURL(event.target.files[0]);
     document.getElementById('outputRGB').src = images.rgb;
@@ -46,15 +12,3 @@ function loadDepthFile(event) {
     images.depth = URL.createObjectURL(event.target.files[0]);
     document.getElementById('outputDepth').src = images.depth;
 };
-
-// function extractFeatures() {
-//     $.post("/image", {
-//         rgb: images.rgb,
-//         depth: images.depth
-//     })
-//     // loadPage('../templates/extraction.html')
-// }
-
-// function showResult() {
-//     loadPage('../templates/result.html')
-// }
