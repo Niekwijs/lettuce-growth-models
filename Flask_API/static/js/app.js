1eabe2e2fec21437ec5d98690d2faca61c526e1e
unknown <mjlp.jacobusse@student.avans.nl>
2023-01-12 14:58:09 +0100

    removed some code duplication and added b64 encoding to storage

--- Flask_API/static/js/app.js
+++ Flask_API/static/js/app.js
@@ -37,15 +37,58 @@ var images = {
 //     });
 // });
 
+//function loadRGBFile(event) {
+//    images.rgb = URL.createObjectURL(event.target.files[0]);
+//    document.getElementById('outputRGB').src = images.rgb;
+//};
+//
+//function loadDepthFile(event) {
+//    images.depth = URL.createObjectURL(event.target.files[0]);
+//    document.getElementById('outputDepth').src = images.depth;
+//};
+
+
+function handleFileUpload(event, storageKey, imageVariable) {
+    // Get the file from the input event
+    const file = event.target.files[0];
+
+    // Create an object URL for the file
+    const objectURL = URL.createObjectURL(file);
+
+    // Assign the object URL to the passed image variable
+    images[imageVariable] = objectURL;
+
+    // Create a new FileReader
+    const reader = new FileReader();
+
+    // Clear out previous data in the document storage
+    localStorage.removeItem(storageKey);
+
+    // Add an event listener for when the file is loaded
+    reader.addEventListener("load", () => {
+        // Get the file"s data as a data URL
+        const dataURL = reader.result;
+
+        // Convert the data URL to a base64 string
+        const base64 = dataURL.split(",")[1];
+
+        // Store the base64 string in the document storage
+        localStorage.setItem(storageKey, base64);
+    });
+
+    // Start reading the file
+    reader.readAsDataURL(file);
+}
+
 function loadRGBFile(event) {
-    images.rgb = URL.createObjectURL(event.target.files[0]);
-    document.getElementById('outputRGB').src = images.rgb;
-};
+    handleFileUpload(event, "rgbFile", "rgb");
+    document.getElementById("outputRGB").src = images.rgb;
+}
 
 function loadDepthFile(event) {
-    images.depth = URL.createObjectURL(event.target.files[0]);
-    document.getElementById('outputDepth').src = images.depth;
-};
+    handleFileUpload(event, "depthFile", "depth");
+    document.getElementById("outputDepth").src = images.depth;
+}
 
 // function extractFeatures() {
 //     $.post("/image", {
