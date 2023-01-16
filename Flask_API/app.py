256dfc291bf03a715e58567bb7097ac78f97e7b3
Joren <63969493+joren961@users.noreply.github.com>
2023-01-16 12:58:54 +0100

    added lin reg, fixed bug. frontend needs variety

--- Flask_API/app.py
+++ Flask_API/app.py
@@ -1,8 +1,7 @@
-import random
-
+import pickle
 import flask
+import pandas as pd
 
-from Flask_API.tabular_predictor import tabular_predictor
 from data_processing import Data
 import tensorflow as tf
 import numpy as np
@@ -11,19 +10,20 @@ app = flask.Flask(__name__, template_folder='./templates')
 
 print('Loading models...')
 
-diameter = tf.keras.models.load_model(r"../Plant_variable_predictor/checkpoints/rgb_augm_reg_diameter/cp-0029.ckpt")
-
-height = tf.keras.models.load_model(r"../Plant_variable_predictor/checkpoints/depth_reg_height/cp-0098.ckpt")
-
-leaf_area = tf.keras.models.load_model("../Plant_variable_predictor/checkpoints/rgb_augm_reg_leafarea/cp-0035.ckpt")
-
-fresh_weight = tf.keras.models.load_model("../Plant_variable_predictor/checkpoints/rgb-d_freshweight/cp-0100.ckpt")
-
-dry_weight = tf.keras.models.load_model("../Plant_variable_predictor/checkpoints/rgb_dryweight/cp-0077.ckpt")
+try:
+    diameter = tf.keras.models.load_model(r"../Plant_variable_predictor/checkpoints/rgb_augm_reg_diameter/cp-0029.ckpt")
+    height = tf.keras.models.load_model(r"../Plant_variable_predictor/checkpoints/depth_reg_height/cp-0098.ckpt")
+    leaf_area = tf.keras.models.load_model("../Plant_variable_predictor/checkpoints/rgb_augm_reg_leafarea/cp-0035.ckpt")
+    fresh_weight = tf.keras.models.load_model("../Plant_variable_predictor/checkpoints/rgb-d_freshweight/cp-0100.ckpt")
+    dry_weight = tf.keras.models.load_model("../Plant_variable_predictor/checkpoints/rgb_dryweight/cp-0077.ckpt")
+    harvest = pickle.load(open('../Forecasting/linear_model-all-varieties.sav', 'rb'))
+except:
+    print("Could not load models. Please check if the models are in the right directories.")
 
 print('Models loaded.')
 
 data = Data()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -49,30 +49,27 @@ def predict_image():
                                                 image_depth=depth_bytes_img,
                                                 requires_normalization=False)
 
-    plant_values = {'diameter': diameter.predict(normalized_images['rgb']),
-                    'height': height.predict(normalized_images['depth']),
-                    'leaf_area': leaf_area.predict(normalized_images['rgb']),
+    plant_values = {'diameter': diameter.predict(normalized_images['rgb'])[0][0],
+                    'height': height.predict(normalized_images['depth'])[0][0],
+                    'leaf_area': leaf_area.predict(normalized_images['rgb'])[0][0],
                     'freshweight': fresh_weight.predict(
-                        np.concatenate((not_normalized_images['rgb'], not_normalized_images['depth']), axis=-1)),
-                    'dryweight': dry_weight.predict(not_normalized_images['rgb'])}
+                        np.concatenate((not_normalized_images['rgb'], not_normalized_images['depth']), axis=-1))[0][0],
+                    'dryweight': dry_weight.predict(not_normalized_images['rgb'])[0][0]}
 
-    print(plant_values)
-
+    print(plant_values['diameter'])
     return flask.render_template('extraction.html', result_plant=plant_values)
 
 
 @app.route('/harvest', methods=['POST'])
 def predict_harvest():
-    features_whitelist = ["Height", "Diameter", "LeafArea", "FreshWeightShoot",
-                          "DryWeightShoot"]  # + ["Variety"] # Variety is not (yet) included in frontend
-    form_values = [flask.request.form.get(field) for field in features_whitelist]
-    prediction = tabular_predictor.predict(form_values)
-
-    # TODO: This does not seem to be the right format, if so, format it correctly
-    predicted_date = prediction[0]
-
-    # predicted_date = '11/09/2001'
-    return flask.render_template('result.html', result_time=predicted_date)
+    features_whitelist = ["FreshWeightShoot", "DryWeightShoot", "Height", "Diameter", "LeafArea"]
+    print(flask.request.form)
+    form_values = [float(flask.request.form.get(field)) for field in features_whitelist]
+    form_values = pd.DataFrame([form_values])
+    print(form_values)
+    prediction = harvest.predict(form_values)
+    # TODO date maken
+    return flask.render_template('result.html', result_time=prediction)
 
 
 if __name__ == '__main__':
